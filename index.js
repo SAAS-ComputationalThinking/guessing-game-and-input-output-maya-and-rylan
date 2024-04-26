f2e9b2f28e3ed0d4815b0832daa241376d500035
Rylan-galante <164915434+Rylan-galante@users.noreply.github.com>
2024-04-26 12:42:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var MagicNumber;
-MagicNumber = 15;
-function numberguess (){
-    var input= document.getElementById ("guessform"). value;
 
-  if (NumberGuess == MagicNumber);
-  <print>"YOU WIN SLAY WHATEVER"</print>
+const magicnumber = Math.floor(Math.random()*101)
+document.getElementById('guesssubmit').addEventListener("click",checkguess);
+function checkguess() {
+    let guesssubmit = document.getElementById("numberinput").value;
+    if(guesssubmit < magicnumber) {
+        document.getElementById('feedback').innerText = "too low";
+    }
+    else if (guesssubmit > magicnumber) {
+        document.getElementById('feedback').innerText = "too high";
+    }    
+    else {
+        document.getElementById('feedback').innerText = "you did it";
+
+    }
+}
+document.getElementById('squaredSubmit').addEventListener("click",numberSquared);
+
+function numberSquared() {
+    let squaredSubmit = document.getElementById("squaredNumberInput").value;
+        document.getElementById('squaredFeedback').innerText = squaredSubmit*squaredSubmit;
 }
